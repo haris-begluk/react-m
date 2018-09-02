c117396546de60bfcb4ac7f746a3686acb035ffa
haris-begluk <harisbegluk@gmail.com>
2018-09-03 00:31:29 +0200

    Exercise decrement button correction

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,10 +5,10 @@ class Counter extends Component {
     //console.log("props", this.props);
     return (
       <div className="row">
-        <div className="col-md-auto">
+        <div className="col-1">
           <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         </div>
-        <div className="col-md-auto">
+        <div className="col">
           <button
             onClick={() => this.props.onIncrement(this.props.counter)}
             className="btn btn-secondary btn-sm"
@@ -19,6 +19,7 @@ class Counter extends Component {
           <button
             onClick={() => this.props.onDecrement(this.props.counter)}
             className="btn btn-secondary btn-sm m-2"
+            disabled={this.props.counter.value === 0 ? "disabled" : ""}
           >
             -
           </button>
